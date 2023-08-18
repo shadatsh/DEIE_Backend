051704c8901d34d83127cdeeda546a34685a640e
arxt9 <136083083+arxt9@users.noreply.github.com>
2023-08-18 23:59:23 +0530

    laboratory routes updated

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,16 @@ const auth = require("./routes/auth");
 const order = require("./routes/order");
 const adminRoutes = require("./routes/admin");
 const formDataRoutes = require("./routes/formDataRoutes");
+const laboratoryRoutes = require("./routes/laboratoryRoutes");
 
 app.use("/api/v1/", products);
 app.use("/api/v1/", auth);
 app.use("/api/v1/", order);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/formdata", formDataRoutes);
+app.use("/formData/:labId", formDataRoutes);
+
+app.use("/api", laboratoryRoutes);
 
 app.use(errorMiddleware);
 
